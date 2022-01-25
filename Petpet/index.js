0f0b90cb293de19e1e2e6cbc2dfca0df60072d0f
Wolfie <Wolfkid200444@gmail.com>
2022-01-24 23:13:37 -0400

    cum

---
+++ Petpet/index.js
@@ -0,0 +1,9 @@
+export default (data) => {
+    return {
+      // This onLoad function will be called when your plugin loads, as the name implies.
+      onLoad() {},
+      // This onUnload function will be called when your plugin unloads, as the name *also* implies.
+      onUnload() {}
+    }
+  }
+  
